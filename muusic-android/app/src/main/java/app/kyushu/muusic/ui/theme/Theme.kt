557e855edb0f56326456df41c4b67fbee723013c
Kayvon-M <142514475+Kayvon-Martinez@users.noreply.github.com>
2023-09-11 21:29:37 -0500

    Setup
    ...

--- muusic-android/app/src/main/java/app/kyushu/muusic/ui/theme/Theme.kt
+++ muusic-android/app/src/main/java/app/kyushu/muusic/ui/theme/Theme.kt
@@ -10,19 +10,26 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF6F11DB),
+    secondary = PurpleGrey40,
+    tertiary = Pink80,
+    onPrimary = Color.White,
+    onSecondary = Color(0xFFfefefe),
+    background = Color(0xFF000000),
+    surface = Color(0xFF1e1e1e),
+    error = Color(0xFFB90000),
+    onError = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Color(0xFF6F11DB),
     secondary = PurpleGrey40,
     tertiary = Pink40
 
@@ -41,7 +48,7 @@ private val LightColorScheme = lightColorScheme(
 fun MuusicTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
